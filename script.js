59f57100a915133068ad6d5520c772b499add32e
JSnyder593 <jaeger_snyder@hotmail.com>
2022-12-15 13:46:07 -0800

    redefined variables, if else for edge cases, revised functions and function order

--- script.js
+++ script.js
@@ -1,89 +1,96 @@
-// Assignment Code
+//arrays of characters
+var lowChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numChars= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+
+//Generates password
+function generatePassword() {
+
+    //confirm password parameters
+    numChar = prompt("How many characters would you like your password to be? (Must be between 8 and 128 characters)");
+
+    if (numChar < 8 || numChar > 128) {
+        alert("Your password must be between 8 and 128 characters!");
+        return;
+    } else if (isNaN(numChar)) {
+        alert("Entry invalid")
+    } else {
+        alert("Password length set!");
+    }
+
+    hasLower = confirm("Would you like lowercase letters?");
+    if (hasLower) {
+        alert("Lowercase included.")
+    } else {
+        alert("Lowercase excluded.")
+    }
+
+    hasUpper = confirm("Would you like uppercase letters?");
+    if (hasUpper) {
+        alert("Uppercase included.")
+    } else {
+        alert("Uppercase excluded.")
+    }
+
+    hasNumbers = confirm("Would you like numbers??");
+    if (hasNumbers) {
+        alert("Numbers included.")
+    } else {
+        alert("Numbers excluded.")
+    }
+
+    hasSpecials = confirm("Would you like special characters?");
+    if (hasSpecials) {
+        alert("Special characters included.")
+    } else {
+        alert("Special characters excluded.")
+
+    }
+
+    if (hasLower === false && hasUpper === false && hasNumbers === false && hasSpecials === false) {
+        alert("You must choose at least one character type!");
+      };
+
+    var allAvail = [];
+
+    //passes all characters based on choices into an array
+    if (hasLower) {
+        allAvail = allAvail.concat(lowChars)
+    }
+    if (hasUpper) {
+        allAvail = allAvail.concat(upChars)
+    }
+    if (hasNumbers) {
+        allAvail = allAvail.concat(numChars)
+    }
+    if (hasSpecials) {
+        allAvail = allAvail.concat(specChars)
+    }
+
+    //sets & defines password
+    var password = ""
+    for (var i = 0; i < numChar; i++) {
+        var randomIndex = [Math.floor(Math.random() * allAvail.length)]
+        password = password + allAvail[randomIndex]
+    }
+    return password;
+};
+
 var generateBtn = document.querySelector("#generate");
 
-//arrays
-var lowCharAvail = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upCharAvail = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbersCharAvail = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specCharAvail = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var allAvail = []
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
-
-function generatePassword() {
-    //get length of password
-    var numChar = prompt("How many characters do you want for your password? *must be 8 - 128 characters*")
-
-    //will it contain lowercase 
-    var lowChar = confirm("Would you like lowercase letters in your password?")
-
-    //will it contain uppercase
-    var upChar = confirm("Would you like uppercase letters in your password?")
-
-    //will it contain numbers
-    var numbersChar = confirm("Would you like numbers in your password?")
-
-    //will it contain special characters
-    var specChar = confirm("Would you like special characters in your password?")
-
-    // if (lowChar && upChar && numChar & specChar) {
-    //     var fullAvail = lowCharAvail.concat(upChar, numsChar, specChar)
-    // }
-
-        if (lowChar) {
-            allAvail = allAvail.concat(lowCharAvail)
-        }
-        if (upChar) {
-            allAvail = allAvail.concat(upCharAvail)
-        }
-        if (numbersChar) {
-            allAvail = allAvail.concat(numbersCharAvail)
-        }
-        if (specChar) {
-            allAvail = allAvail.concat(specCharAvail)
-        }
-        console.log(allAvail)
-
-        for (let i = 0; i < numChar; i++) {
-            var password = (Math.floor(Math.random()) * allAvail)
-            return password
-    }
-}
-
 
 generateBtn.addEventListener("click", writePassword);
 
-Math.floor((Math.random() * 128) + 1);
-Math.floor(Math.random() * 26);
-Math.floor(Math.random() * 26);
-Math.floor(Math.random() * 10);
-Math.floor(Math.random() * 10);
 
 
 
 
 
-
-
-
-//function that asks for the parameters of your password
-
-
-// function createPassword() {
-//     let numChar = [Array.from({length: 128}, (_, i) => i + 1)]
-//     let lowChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//     let upChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//     let numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-//     let specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-
-// }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
